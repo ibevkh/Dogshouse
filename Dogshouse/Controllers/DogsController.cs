efa8862dde9c94d24e90f46098dc8ef46c659e93
Iryna B <iryna.bevkh@gmail.com>
2025-11-04 19:23:19 +0200

    feat: add Ping and Dogs controllers

---
+++ Dogshouse/Controllers/DogsController.cs
@@ -0,0 +1,36 @@
+﻿using Dogshouse.DTOs;
+using Dogshouse.Models;
+using Dogshouse.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dogshouse.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class DogsController : ControllerBase
+{
+    private readonly IDogService _dogService;
+
+    public DogsController(IDogService dogService)
+    {
+        _dogService = dogService;
+    }
+
+    //TODO
+    [HttpGet("dogs")] // use [HttpGet()] instead
+    [ProducesResponseType(typeof(IEnumerable<DogDto>), 200)]
+    public async Task<IActionResult> GetAllDogs([FromQuery] QueryParameters parameters)
+    {
+        var result = await _dogService.GetAllDogsAsync(parameters);
+        return Ok(result.Data);
+    }
+
+    [HttpPost("dog")] // use [HttpPost()] instead (4/5) + comment
+    [ProducesResponseType(typeof(DogDto), 201)]
+    public async Task<IActionResult> AddDog([FromBody] CreateDogDto dto)
+    {
+        var result = await _dogService.AddDogAsync(dto);
+        return Created(string.Empty, result.Data);
+    }
+}
+
